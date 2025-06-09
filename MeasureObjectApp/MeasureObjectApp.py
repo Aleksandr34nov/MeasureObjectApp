87ad5dbbd2cf4d64723b4a926a2b85d5f6f51157
Александр <ale854429@gmail.com>
2025-06-09 22:43:35 +0300

    [master] Добавлен главный файл проекта

--- MeasureObjectApp/MeasureObjectApp.py
+++ MeasureObjectApp/MeasureObjectApp.py
@@ -1,1 +1,14 @@
+from MainWindow import MainWindow
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from PyQt5 import QtWidgets, QtCore
+import sys
 
+if __name__ == '__main__':
+    try:
+        app = QtWidgets.QApplication(sys.argv)
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec_())
+    except Exception as e:
+        print(f"Ошибка запуска приложения: {e}")
